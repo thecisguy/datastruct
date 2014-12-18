7f8e3102d207dbbff1149b72fe6351eeb5e0f2ba
Blake Lowe <b.f.lowe@csuohio.edu>
2014-12-18 09:45:08 -0500

    implement initializers

--- priqueue.c
+++ priqueue.c
@@ -13,6 +13,43 @@ struct PRIQUEUE {
 };
 
 static PRIQUEUENODE pair(PRIQUEUENODE p, PRIQUEUENODE q,
+		int (*compar)(const void *, const void *));
+static PRIQUEUENODE new_priqueuenode(void *data);
+
+static PRIQUEUENODE new_priqueuenode(void *data) {
+	PRIQUEUENODE npqn = (PRIQUEUENODE)
+		malloc(sizeof(struct PRIQUEUENODE));
+	if (!npqn)
+		goto out;
+
+	npqn->left = NULL;
+	npqn->right = NULL;
+	npqn->data = data;
+
+out:
+	return npqn;
+}
+
+PRIQUEUE pq_init(int (*compar)(const void *, const void *)) {
+	PRIQUEUE npq = (PRIQUEUE) malloc(sizeof(struct PRIQUEUE));
+	if (!npq)
+		goto out;
+
+	npq->binqueue = ll_init();
+	if (!npq->binqueue)
+		goto out_bq;
+	npq->compar = compar;
+
+	goto out;
+
+out_bq:
+	free(npq);
+	npq = NULL;
+out:
+	return npq;
+}
+
+static PRIQUEUENODE pair(PRIQUEUENODE p, PRIQUEUENODE q,
 		int (*compar)(const void *, const void *)) {
 	PRIQUEUENODE t;
 	if (compar(p->data, q->data) < 0) {
@@ -25,3 +62,5 @@ static PRIQUEUENODE pair(PRIQUEUENODE p, PRIQUEUENODE q,
 		return p;
 	}
 }
+
+
